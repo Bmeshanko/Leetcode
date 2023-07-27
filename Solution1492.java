a3b384c0b22d4a5d96675b2140a33dd9bb8a53df
Bmeshanko <bmeshank@purdue.edu>
2023-07-26 22:34:38 -0400

    Leetcode 1492

---
+++ Solution1492.java
@@ -0,0 +1,11 @@
+class Solution1492 {
+    public int kthFactor(int n, int k) {
+        int originalK = k;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) k--;
+            if (k == 0) return i;
+        }
+
+        return -1;
+    }
+}
